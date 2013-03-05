eef4f5068afec7e0766f1f20e306b515ca79c266
Ryan Kuester <rkuester@insymbols.com>
2013-03-05 23:35:42 +0000

    Add a package which hooks the systemd at-boot.target

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 BIN=${DESTDIR}/usr/bin
 FIRSTBOOT=${DESTDIR}/usr/lib/firstboot/run.d
-install -d $BIN $FIRSTBOOT
+LIB=${DESTDIR}/usr/lib/dpkg-autoconfigure
+ATBOOT=debian/dpkg-autoconfigure-at-boot
+install -d $BIN $FIRSTBOOT $LIB $ATBOOT
 install dpkg-autoconfigure $BIN
 install -d ${DESTDIR}/usr/share/dpkg-autoconfigure/selections.d
 install -m 644 20-ptux.selections ${DESTDIR}/usr/share/dpkg-autoconfigure/selections.d
 install 40-mountkernfs.sh ${FIRSTBOOT}
 install 95-reboot.sh ${FIRSTBOOT}
 ln -sf /usr/bin/dpkg-autoconfigure ${FIRSTBOOT}/50-dpkg-autoconfigure
+install at-boot $LIB
+ln -sf /usr/lib/dpkg-autoconfigure/at-boot ${ATBOOT}/at-boot
